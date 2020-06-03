ebf364443f66d8e5d0dbe25ffe215e44e1f965ac
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-06-03 16:10:25 +0200

    First commit. Added html.css.app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+
+from os import path
+if path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'pulpRecordsDB'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
+@app.route('/')
+def hello():
+    return"hello world"
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=(os.environ.get('PORT')),
+            debug=True)
