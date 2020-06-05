f3b221e34163b928cfcc8b6287ab8827baffc4d1
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-06-05 14:32:05 +0200

    added code to app.py, html and connected to heroku

--- app.py
+++ app.py
@@ -17,9 +17,43 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return "hello world"
+@app.route('/index')
+def index():
+    return render_template("index.html")
 
+@app.route('/records')
+def records():
+    return render_template("records.html")
+
+
+@app.route('/add_records')
+def add_records():
+    return render_template("addrecords.html")
+
+
+@app.route('/profile')
+def profile():
+    return render_template("profile.html")
+
+
+@app.route('/log_out')
+def log_out():
+    return render_template("logout.html")
+
+
+
+@app.route('/get_reviews')
+def get_reviews():
+    return render_template('records.html', 
+                           reviews=mongo.db.reviews.find())
+
+
+
+@app.route('/insert_genra', methods=['POST'])
+def insert_genra():
+    tasks =  mongo.db.genra
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_reviews'))
 
 
 
