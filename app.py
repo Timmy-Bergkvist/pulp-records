7b7f9d73a374800dca1ca26d31585693fd758327
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2020-06-11 11:11:13 +0200

    setting up right form in app.py and html to create a login and logout function

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
-import re
-import logging
 from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
-from flask_login import LoginManager, current_user, login_user, logout_user, login_required
-from bson.objectid import ObjectId
-from forms import LoginForm, RegistrationForm
+from flask_mongoengine import MongoEngine, Document
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 from werkzeug.security import generate_password_hash, check_password_hash
-from user import User
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 
 
@@ -20,11 +20,30 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'pulpRecordsDB'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
-
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-
 mongo = PyMongo(app)
+
+db = MongoEngine(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+class User(UserMixin, db.Document):
+    meta = {'collection': '<---YOUR_COLLECTION_NAME--->'}
+    email = db.StringField(max_length=30)
+    password = db.StringField()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.objects(pk=user_id).first()
+
+class RegForm(FlaskForm):
+    email = StringField('email',  validators=[InputRequired(), Email(message='Invalid email'), Length(max=30)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=20)])
+
+
 
 @app.route('/')
 @app.route('/index')
@@ -33,6 +52,7 @@ def index():
 
 
 @app.route('/get_genra')
+@login_required
 def get_genra():
     return render_template('records.html', 
                            reviews=mongo.db.reviews.find())
@@ -61,7 +81,7 @@ def records():
 @app.route('/profile')
 @login_required
 def profile():
-    return render_template("profile.html")
+    return render_template("profile.html", name=current_user.email)
 
 
 #Registration
@@ -76,31 +96,16 @@ def register():
                 hashpass = generate_password_hash(form.password.data, method='sha256')
                 hey = User(form.email.data,hashpass).save()
                 login_user(hey)
-                return redirect(url_for('dashboard'))
+                return redirect(url_for('profile'))
     return render_template('register.html', form=form)
 
 
 #Login
 
-class User(UserMixin, db.register):
-    meta = {'collection': '<---YOUR_COLLECTION_NAME--->'}
-    email = db.register(max_length=30)
-    password = db.register()
-@login_manager.user_loader
-def load_user(user_id):
-    return User.objects(pk=user_id).first()
-
-
-class RegForm(FlaskForm):
-    email = StringField('email',  validators=[InputRequired(), Email(message='Invalid email'), Length(max=30)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=20)])
-
-
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated == True:
-        return redirect(url_for('dashboard'))
+        return redirect(url_for('profile'))
     form = RegForm()
     if request.method == 'POST':
         if form.validate():
@@ -108,16 +113,18 @@ def login():
             if check_user:
                 if check_password_hash(check_user['password'], form.password.data):
                     login_user(check_user)
-                    return redirect(url_for('dashboard'))
+                    return redirect(url_for('profile'))
     return render_template('login.html', form=form)
 
 
+
+#Logout
 
 @app.route('/logout', methods = ['GET'])
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('login'))
 
 
 
